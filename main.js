9d0dd733bbf915edb90745d8bbf6aea5018ebb1c
ayaangupta <89456815+ayaangupta@users.noreply.github.com>
2023-09-09 08:42:11 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function cat_page(){
+    window.location = "cat_page.html";
+}
+
+function dog_page(){
+    window.location = "dog_page.html";
+}
+
+function phone_page(){
+    window.location = "phone.html";
+}
+
+function suit_page(){
+    window.location = "suitcase_page.html";
+}
+
+function ipad_page(){
+    window.location = "ipad.html";
+}
